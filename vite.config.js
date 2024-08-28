831643cd3ac2de30ba5ccf08e122c398ceece20b
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-08-28 14:43:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './build', // Update this line
+    emptyOutDir: true,
+  },
   base: "/shoppingreact",
   plugins: [react()],
-})
+
+});
